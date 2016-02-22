3392b975d8741f05f653d73658a86b73aca6a496
Manabu Morita <tsuwako+github@gmail.com>
2016-02-22 22:55:22 +0900

    create constants

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 'use strict';
 var koa = require('koa')
-  , serve = require('koa-static');
+  , serve = require('koa-static')
+  ;
 
 var options = {
-  PORT: 3000
+  PORT: 3000,
+  ROOT: '/public',
+  HOME: '/html/index.html'
 };
 
 var app = koa();
 
-app.use(serve(__dirname + '/public'));
+app.use(serve(__dirname + options.ROOT));
 
 app.use(function *(next) {
-    this.redirect('/html/index.html');
+    this.redirect(options.HOME);
 });
 
 app.listen(options.PORT);
